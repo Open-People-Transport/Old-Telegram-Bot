923e833cfb02483976fced39594cbcdda0d1a967
Dmitry Semenov <lonelyteapot@gmail.com>
2022-07-04 13:40:09 +0300

    Fix alignment

--- src/pages/routes.py
+++ src/pages/routes.py
@@ -14,12 +14,14 @@ class RoutesPage(Page):
             if not i % COLUMNS:
                 keyboard.append([])
             button = InlineKeyboardButton(
-                route.number, callback_data=f"route {route.id}"
+                f"{route.number: ^10}", callback_data=f"route {route.id}"
             )
             keyboard[-1].append(button)
 
-        if len(keyboard[-1]) == COLUMNS:
-            keyboard.append([])
-        keyboard[-1].append(InlineKeyboardButton("Назад", callback_data="home"))
+        keyboard[-1].extend(
+            [InlineKeyboardButton(" ", callback_data="routes")]
+            * (COLUMNS - len(keyboard[-1]))
+        )
+        keyboard.append([InlineKeyboardButton("Назад", callback_data="home")])
 
         self.keyboard = InlineKeyboardMarkup(keyboard)
